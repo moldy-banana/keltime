eb5aabaadb8e4a0ddccf5dd62d44890d6cb9c3f4
lek. <wenellie253@gmail.com>
2026-02-16 23:54:33 -0500

    first v2 "there was an attempt" push

---
+++ js/marquee.js
@@ -0,0 +1,2 @@
+document.write('<div class="nav-main-spacer"> <div class="marquee3k" data-speed="2" data-reverse="false" data-pausable="true"><p>/ we take pictures of the dove, wondering why we cannot hear their song // keltime // welcome /</p></div></div>');
+document.write('<script src="/js/marquee3k.js"></script>');
