6398eed9fffefa373816433d178c2f37cdc5f1f1
moldy-banana <wenellie253@gmail.com>
2026-08-31 23:27:34 -0400

    v2.5 the world in a window- updated index.html, new diary entries, new buttons, etc.

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,1 @@
-document.write('  <footer  class="about footer"><p class="text-spacing center"> v2 "there was an attempt." // do not copy //  <a href="credits.html" class="no-border">credits</a>  // ©2025 kel </p></footer>')
+document.write('  <footer  class="about footer"><p class="text-spacing center"> v2.5 "the world in a window." // do not copy //  <a href="credits.html" class="no-border"> credits </a>  // ©2025 kel </p></footer>')
